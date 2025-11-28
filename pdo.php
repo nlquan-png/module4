ecead373f0807a9394637c3548ce7145c4c40f40
nlquan-png <nlquan@vku.udn.vn>
2025-11-28 09:54:04 +0700

    Update PDO connection parameters for database access

--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc', 
-   'fred', 'zap');
+$pdo = new PDO('mysql:host=localhost;dbname=misc', 
+   'root', '');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

1ead95e22e27704b07761a22d6ec44b4e4bf377d
nlquan-png <nlquan@vku.udn.vn>
2025-11-28 09:22:00 +0700

    Update title in index.php to include hash

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 
-    <title>Ngô Lê Quân nlquan@vku.udn.vn</title>
+    <title>Ngô Lê Quân nlquan@vku.udn.vn |4c3da009 </title>
 </head>
 <body>
 <div class="container">

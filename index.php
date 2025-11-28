7bdff2a299f9b0f36bdf03c70a2d6ee74ff56150
nlquan-png <nlquan@vku.udn.vn>
2025-11-28 10:31:23 +0700

    Remove Bootstrap CSS and script from index.php
    Removed Bootstrap CSS links and a script tag.

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 <html>
 <head>
 <title>Ngô Lê Quân nlquan@vku.udn.vn | 38d08 - Autos Database</title>
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
 </head>
 <body>
 <div class="container">
@@ -28,6 +21,5 @@ Attempt to go to
 <a href="https://github.com/nlquan-png/module4/tree/main" target="_blank">Specification for this Application</a>
 </p>
 </div>
-<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"version":"2024.11.0","token":"256a5db7232d48239a0fb56411d05bd3","r":1,"server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}' crossorigin="anonymous"></script>
 </body>
 </html>

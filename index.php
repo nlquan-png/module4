5f812ae1456c8b466c1cf3f031132c2e4c6484d3
nlquan-png <nlquan@vku.udn.vn>
2025-11-28 10:43:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
 <div class="container">
 <h1>Welcome to Autos Database</h1>
 <p>
-<a href="login.php">Please Log In</a>
+<a href="https://github.com/nlquan-png/module4/blob/main/login.php">Please Log In</a>
 </p>
 <p>
 Attempt to go to 
-<a href="view.php">view.php</a> without logging in - it should fail with an error message.
+<a href="https://github.com/nlquan-png/module4/blob/main/view.php">view.php</a> without logging in - it should fail with an error message.
 </p>
 <p>
 Attempt to go to 
-<a href="add.php">add.php</a> without logging in - it should fail with an error message.
+<a href="https://github.com/nlquan-png/module4/blob/main/add.php">add.php</a> without logging in - it should fail with an error message.
 </p>
 <p>
 <a href="https://github.com/nlquan-png/module4/blob/main/" target="_blank">Specification for this Application</a>

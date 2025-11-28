25fac7854bc718f7eea81d952c893ca0f9de75a6
nlquan-png <nlquan@vku.udn.vn>
2025-11-28 09:25:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <a href="add.php">add.php</a> without logging in - it should fail with an error message.
     </p>
     <p>
-        <a href="https://github.com/nlquan-png/module4/edit/main/index.php" target="_blank">Specification for this Application</a>
+        <a href="https://github.com/nlquan-png/module4/blob/main/index.php" target="_blank">Specification for this Application</a>
     </p>
 </div>
 </body>

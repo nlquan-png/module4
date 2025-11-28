463fa4d55ecf90b526f0698f1a6d265cd3aae089
nlquan-png <nlquan@vku.udn.vn>
2025-11-28 09:33:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<title>Ngô Lê Quân - Autos Database</title>
 
-    <title>Ngô Lê Quân nlquan@vku.udn.vn |4c3da009 </title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
 </head>
 <body>
 <div class="container">
-    <h1>Welcome to Autos Database</h1>
-    <p>
-        <a href="login.php">Please Log In</a>
-    </p>
-    <p>
-        Attempt to go to
-        <a href="view.php">view.php</a> without logging in - it should fail with an error message.
-    </p>
-    <p>
-        Attempt to go to
-        <a href="add.php">add.php</a> without logging in - it should fail with an error message.
-    </p>
-    <p>
-        <a href="https://github.com/nlquan-png/module4/blob/main/index.php" target="_blank">Specification for this Application</a>
-    </p>
+<h1>Welcome to Autos Database</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="view.php">view.php</a> without logging in - it should fail with an error message.
+</p>
+<p>
+Attempt to go to 
+<a href="add.php">add.php</a> without logging in - it should fail with an error message.
+</p>
+<p>
+<a href="https://www.wa4e.com/assn/autosess/" target="_blank">Specification for this Application</a>
+</p>
 </div>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"version":"2024.11.0","token":"256a5db7232d48239a0fb56411d05bd3","r":1,"server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}' crossorigin="anonymous"></script>
 </body>
 </html>
